0c49d35853d69dabb62a8c27f26f7ae87d523dc1
Steven Clontz <steven.clontz@gmail.com>
2022-06-02 16:31:15 +0000

    remove dev suffix from checkit version in app footer

--- viewer/src/App.svelte
+++ viewer/src/App.svelte
@@ -42,7 +42,7 @@
                 <em>
                     Randomized exericse bank powered by
                     <a target="_blank" href="https://checkit.clontz.org">CheckIt</a>
-                    v0.2.0dev0
+                    v0.2.0
                 </em>
             </small>
         </p>
