d0cba27b1b38a6cad274f17b57c65e6904b5ef67
Steven Clontz <steven.clontz@gmail.com>
2022-06-30 19:58:04 +0000

    set alpha verisons

--- viewer/src/App.svelte
+++ viewer/src/App.svelte
@@ -42,7 +42,7 @@
                 <em>
                     Randomized exericse bank powered by
                     <a target="_blank" href="https://checkit.clontz.org">CheckIt</a>
-                    v0.2.1
+                    v0.2.2a0
                 </em>
             </small>
         </p>
