685cd924869f55e27d2ed42243a59a844b3a06d5
Steven Clontz <steven.clontz@gmail.com>
2022-07-03 20:00:05 +0000

    set alpha

--- viewer/src/App.svelte
+++ viewer/src/App.svelte
@@ -42,7 +42,7 @@
                 <em>
                     Randomized exericse bank powered by
                     <a target="_blank" href="https://checkit.clontz.org">CheckIt</a>
-                    v0.2.2
+                    v0.2.3a0
                 </em>
             </small>
         </p>
