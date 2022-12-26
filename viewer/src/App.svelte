459289fa8b0b6a24bf9ae5b1786606037b2ba479
Steven Clontz <steven.clontz@gmail.com>
2022-12-26 15:09:28 +0000

    update version

--- viewer/src/App.svelte
+++ viewer/src/App.svelte
@@ -42,7 +42,7 @@
                 <em>
                     Randomized exericse bank powered by
                     <a target="_blank" href="https://checkit.clontz.org">CheckIt</a>
-                    v0.2.4
+                    v0.2.5
                 </em>
             </small>
         </p>
