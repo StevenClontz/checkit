4dcb4a0dd3094b3439c3a6d7d2d896ee3b71a49a
Steven Clontz <steven.clontz@gmail.com>
2023-03-01 18:57:30 +0000

    update version

--- viewer/src/App.svelte
+++ viewer/src/App.svelte
@@ -42,7 +42,7 @@
                 <em>
                     Randomized exericse bank powered by
                     <a target="_blank" href="https://checkit.clontz.org">CheckIt</a>
-                    v0.2.5
+                    v0.2.6
                 </em>
             </small>
         </p>
