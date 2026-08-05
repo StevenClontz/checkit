f62e567f46f636917cc9fa6033263ef9082a7e50
Steven Clontz <steven.clontz@gmail.com>
2026-08-05 18:56:48 +0000

    randomize order of MCQ choices in viewer

--- viewer/src/spatext/Elements/Knowl.svelte
+++ viewer/src/spatext/Elements/Knowl.svelte
@@ -12,6 +12,14 @@
         if (p.parentElement!=null && p.parentElement.tagName=="knowl") return isInExercise(p.parentElement)
         return false
     }
+    const randomChoices = (e:Element) => {
+        let choices = [...e.querySelectorAll(":scope > outtro")]
+        for (let i=choices.length-1; i>0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [choices[i], choices[j]] = [choices[j], choices[i]];
+        }
+        return choices
+    }
     let outtroLabel:string
     let partLabel:string
     let knowlLabel:string
@@ -68,7 +76,7 @@
             {#if knowl.querySelectorAll(":scope > outtro[distractor='true']").length > 1}
                 <h5>Choices:</h5>
                 <ol class="choices">
-                {#each [...knowl.querySelectorAll(":scope > outtro")] as outtro}
+                {#each randomChoices(knowl) as outtro}
                     <li><Content content={outtro}/></li>
                 {/each}
                 </ol>
